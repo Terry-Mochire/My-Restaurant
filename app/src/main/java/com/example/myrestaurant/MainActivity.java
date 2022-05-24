3857f59f0971abafbb4c29cc0fc0a39e8a4fc343
Terry <terrybmochire@gmail.com>
2022-05-25 02:11:15 +0300

    Pass location data between mainactivity and restaurant activity.

--- app/src/main/java/com/example/myrestaurant/MainActivity.java
+++ app/src/main/java/com/example/myrestaurant/MainActivity.java
@@ -2,26 +2,33 @@ package com.example.myrestaurant;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     private Button mFindRestaurantsButton;
+    private EditText mLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
         mFindRestaurantsButton = (Button)findViewById(R.id.findRestaurantsButton);
 
         mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+
+                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class );
+                String location = mLocationEditText.getText().toString();
+                intent.putExtra("location", location);
+                startActivity(intent);
             }
         });
     }
--- app/src/main/java/com/example/myrestaurant/RestaurantsActivity.java
+++ app/src/main/java/com/example/myrestaurant/RestaurantsActivity.java
@@ -2,13 +2,20 @@ package com.example.myrestaurant;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class RestaurantsActivity extends AppCompatActivity {
+    private TextView mlocationTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
+        mlocationTextView = (TextView) findViewById(R.id.locationTextView);
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
+        mlocationTextView.setText("Here are all the restaurants near: " + location);
     }
 }
